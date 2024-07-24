cd86683fdb30da10b3b59c901a7d10b53ad26277
Takuma Mizuno <34719035ict.nitech.ac.jp>
2024-07-24 06:39:35 +0000

    fix fibonacci

--- testFibonacci.py
+++ testFibonacci.py
@@ -3,14 +3,14 @@ from recursive import fibonacci
 
 class TestFibonacci(unittest.TestCase):
 
-    def test_factotial(self):
+    def test_fibonacci(self):
         val = fibonacci(10)
-        self.assertEqual(55,val)
+        self.assertEqual(55, val)
 
-    def test_factotial_a0(self):
+    def test_fibonacci_a0(self):
         val = fibonacci(0)
-        self.assertEqual(1,val)
-    
-    def test_factotial_a1(self):
+        self.assertEqual(0, val)
+
+    def test_fibonacci_a1(self):
         val = fibonacci(1)
-        self.assertEqual(1,val)
+        self.assertEqual(1, val)

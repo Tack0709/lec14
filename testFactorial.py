ebbbf2694533c5b643ffcb9be5162949756aeef2
Takuma Mizuno <34719035ict.nitech.ac.jp>
2024-07-24 06:39:53 +0000

    clean factorial

--- testFactorial.py
+++ testFactorial.py
@@ -5,12 +5,12 @@ class TestFactorial(unittest.TestCase):
 
     def test_factotial(self):
         val = factorial(4)
-        self.assertEqual(24,val)
+        self.assertEqual(24, val)
 
     def test_factotial_a0(self):
         val = factorial(0)
-        self.assertEqual(1,val)
-    
+        self.assertEqual(1, val)
+
     def test_factotial_a1(self):
         val = factorial(1)
-        self.assertEqual(1,val)
+        self.assertEqual(1, val)
